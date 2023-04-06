1df99f6a242c51e7252faaa78bcc929312b46cc1
Jonas <sturmente@proton.me>
2023-04-06 17:27:39 +0200

    Added automatic redirect when not logged in

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
 import { join as path } from "path";
 
+const ignoreLoggedIn = ["login", "api", ".css", ".js"];
+
 const app = express();
+
+app.use(cookieParser());
+
+app.all("*", (req, res) => {
+	console.log(req.url);
+	if (!req.cookies.loggedIn && ignore(req.url)) {
+		res.redirect("/login");
+		return;
+	}
+
+	if (req.next) req.next();
+});
 
 app.use(express.static(path(__dirname, "public")));
 
@@ -13,3 +28,13 @@ app.all("*", (req, res) => {
 app.listen(process.env.PORT, () => {
 	console.log("Listening to port", process.env.PORT);
 });
+
+function ignore(url: string): boolean {
+	let ignore = false;
+	ignoreLoggedIn.forEach((s) => {
+		if (url.includes(s)) {
+			ignore;
+		}
+	});
+	return ignore;
+}
