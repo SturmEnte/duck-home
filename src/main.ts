9851acd7227f81858b9d993232fa3fdd7c3f8f64
Jonas <sturmente@proton.me>
2023-04-06 16:48:06 +0200

    Added 404 page and added static component

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import { join as path } from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.use(express.static(path(__dirname, "public")));
+
+app.all("*", (req, res) => {
+	res.sendFile(path(__dirname, "public", "404/404.html"));
 });
 
 app.listen(process.env.PORT, () => {
