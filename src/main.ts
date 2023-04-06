d3ab13aaff94f1a69095410a73620d239ca6f063
Jonas <sturmente@proton.me>
2023-04-06 16:38:45 +0200

    Basic web server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+	console.log("Listening to port", process.env.PORT);
+});
