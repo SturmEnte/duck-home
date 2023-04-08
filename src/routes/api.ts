9f8d1e176b2959f97282e27cd0e8d483ed54ec19
Jonas <sturmente@proton.me>
2023-04-08 23:18:58 +0200

    Api authentication

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,13 +3,30 @@ import { Router, json } from "express";
 import signup from "./auth/signup";
 import login from "./auth/login";
 import user from "./user";
+import Token from "../models/Token";
 
 const router = Router();
 
 router.use(json());
 
+// Authentication
 router.use("/signup", signup);
 router.use("/login", login);
+
+// Check if user is authenticated
+router.all("*", async (req, res, next) => {
+	if (!req.headers.authorization) {
+		res.sendStatus(401);
+		return;
+	}
+
+	if (!(await Token.exists({ token: req.headers.authorization }))) {
+		res.sendStatus(401);
+		return;
+	}
+
+	next();
+});
 
 router.use("/user", user);
 
