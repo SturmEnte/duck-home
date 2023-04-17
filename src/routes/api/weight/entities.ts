6e415a63139ea1f747ef33c8c74469773c51f55e
Jonas <sturmente@proton.me>
2023-04-17 18:43:49 +0200

    entities api route

--- src/routes/api/weight/entities.ts
+++ src/routes/api/weight/entities.ts
@@ -1,7 +1,25 @@
 import { Router } from "express";
 
+import WeightEntity from "../../../models/WeightEntity";
+
+interface Entity {
+	name: string;
+	unit: string;
+	weight: { date: Date; weight: number }[];
+}
+
 const router = Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+	let entities: Entity[] = [];
+	(await WeightEntity.find({ user_id: req.headers.authorization?.split(".")[1] })).forEach((entity) => {
+		entities.push({
+			name: entity.name,
+			unit: entity.unit,
+			weight: entity.weight,
+		});
+	});
+	res.json(entities);
+});
 
 export default router;
