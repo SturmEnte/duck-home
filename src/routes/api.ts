e833cc7daf6ab8f2abcc05bee391279e7491c30c
Jonas <sturmente@proton.me>
2023-04-17 18:09:40 +0200

    Added backend routes for the weight module

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,8 +3,13 @@ import { Router, json } from "express";
 import signup from "./api/auth/signup";
 import login from "./api/auth/login";
 import logout from "./api/auth/logout";
+
 import user from "./api/user";
 import updates from "./api/updates";
+
+import createEntity from "./api/weight/createEntity";
+import entities from "./api/weight/entities";
+import setEntry from "./api/weight/setEntry";
 
 import Token from "../models/Token";
 
@@ -34,5 +39,10 @@ router.all("*", async (req, res, next) => {
 
 router.use("/user", user);
 router.use("/updates", updates);
+
+// Weight
+router.use("/createEntity", createEntity);
+router.use("/entities", entities);
+router.use("/setEntry", setEntry);
 
 export default router;
