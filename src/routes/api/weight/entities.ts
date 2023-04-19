c93db8720ab27879c7131d78c12b530e949cce57
Jonas <sturmente@proton.me>
2023-04-19 19:01:07 +0200

    Remove not needed data from weight entries

--- src/routes/api/weight/entities.ts
+++ src/routes/api/weight/entities.ts
@@ -5,7 +5,12 @@ import WeightEntity from "../../../models/WeightEntity";
 interface Entity {
 	name: string;
 	unit: string;
-	weight: { date: number; weight: number }[];
+	weight: WeightEntry[];
+}
+
+interface WeightEntry {
+	weight: number;
+	date: number;
 }
 
 const router = Router();
@@ -13,10 +18,19 @@ const router = Router();
 router.get("/", async (req, res) => {
 	let entities: Entity[] = [];
 	(await WeightEntity.find({ user_id: req.headers.authorization?.split(".")[1] })).forEach((entity) => {
+		let weight: WeightEntry[] = [];
+
+		entity.weight.forEach((w) => {
+			weight.push({
+				weight: w.weight,
+				date: w.date,
+			});
+		});
+
 		entities.push({
 			name: entity.name,
 			unit: entity.unit,
-			weight: entity.weight,
+			weight: weight,
 		});
 	});
 	res.json(entities);
