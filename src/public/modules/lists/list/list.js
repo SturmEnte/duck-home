da6daf371782036a6c24d0ad8f18094206c99efa
Jonas <sturmente@proton.me>
2023-11-05 22:41:52 +0100

    Display all list entries from a list

--- src/public/modules/lists/list/list.js
+++ src/public/modules/lists/list/list.js
@@ -1,1 +1,19 @@
-document.getElementById("content").innerHTML = window.location.hash;
+const LIST_ID = window.location.hash.slice(1);
+
+fetch("/api/lists/entries/entries?list_id=" + encodeURIComponent(LIST_ID), {
+	method: "get",
+	headers: {
+		Authorization: localStorage.getItem("token"),
+	},
+})
+	.then((res) => {
+		res.json().then((data) => {
+			data.forEach((entry) => {
+				document.getElementById("content").innerHTML += `<br/>${entry.title}`;
+			});
+		});
+	})
+	.catch((err) => {
+		console.error("Error while fetchting list entries:");
+		console.error(err);
+	});
